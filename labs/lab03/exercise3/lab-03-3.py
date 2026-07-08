af889448650feb5b34eca1d70267ed6edea004fe
suhadah8 <nrlsuhadahh@gmail.com>
2026-07-08 12:30:05 +0800

    exercise

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte / 1024
+gigabyte = megabyte / 1024
+terabyte = gigabyte / 1024
+petabyte = terabyte / 1024
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
---
+++ labs/lab03/exercise4/lab-3-4.py
@@ -0,0 +1,6 @@
+servicecharge = 3
+pagecost = 0.2
+pages = int(input())
+totalpayment = pages * pagecost + servicecharge
+print(totalpayment)
+ 
