dc0e1bec91320e9cf78595a5bb09624969bbd413
suhadah8 <nrlsuhadahh@gmail.com>
2026-07-15 12:04:07 +0800

    exerciseeeeeeeeeeee33333e3e3e33e3e3twoooooooooo0o0o0o0o

--- labs/lab04/exercise2/lab-4-2.py
+++ labs/lab04/exercise2/lab-4-2.py
@@ -5,5 +5,5 @@ else:
     if annualincome >= 100000:
         totaltax = (50000 * 0) + (50000 * 0.01) + ((annualincome - 100000) * 0.02)
     else:
-        totaltax = (50000 * 0.0) + annualincome - 50000 * 0.01
+        totaltax = (50000 * 0.0) + (annualincome - 50000 * 0.01)
 print(totaltax)
