86a9a4cfde2b65c87350e7cc46dc3caed18de06f
suhadah8 <nrlsuhadahh@gmail.com>
2026-07-15 11:38:15 +0800

    EXERCISEEEEEEEEEEEEEEEEEEEEONEEEEEEEEEEEEEEEEE

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -3,7 +3,7 @@ if kWh <= 100:
     bill = 0.3 * kWh
 else:
     if kWh > 100 and kWh <= 200:
-        bill = 100 * 0.3 + 100 - kWh * 0.5
+        bill = 100 * 0.3 + kWh - 100 * 0.5
     else:
         bill = 100 * 0.3 + 100 * 0.5 + kWh - 200 * 0.75
 print(bill)
