236db489b73654eb64a729a829a8b54171295aba
suhadah8 <nrlsuhadahh@gmail.com>
2026-08-12 12:25:35 +0800

    SHD

---
+++ labs/labTest/lab-test-2.py
@@ -0,0 +1,19 @@
+#declare variable
+full_mark = 2*10
+name = "Ahmad Bin Abu"
+matric_no = "MS2025123499"
+
+#print output ^.^ 
+print(f"Name: {name}\t\t\tMatric. No: {matric_no}\n\n\n"
+      "*\t\t\t*\n"
+      "**\t\t**\n"
+      "***\t***\n"
+      "********\n"
+      "***\t***\n"
+      "**\t\t**\n"
+      "*\t\t\t*\n\n\n"
+      "This is my\n"
+      "\tsecond\n"
+      "\t\tassignment\n"
+      f"I want 2x10 marks, which is {full_mark} full marks\n"
+)
